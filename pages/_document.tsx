e0396ac0ee52ad1a8e6f20f22835041fee6aa41a
Ashish_jha01 <Ashish.jha@optimiser.com>
2024-09-26 23:03:01 +0530

    Todolist

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>AiTodoList</title>
+        <link rel="icon" href="/ai-logo.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
